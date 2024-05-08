ff11c75519c0c91e4e0f96f400558e4dc9afc23b
theanishtar <dangtt135@gmail.com>
2024-05-08 13:35:04 +0700

    refactor: refac function claerMarkTag

--- js/search.js
+++ js/search.js
@@ -1,20 +1,24 @@
 let debounceTimer;
 
+const clearMarkTag = () => {
+  const contents = document.querySelectorAll('.content');
+  contents.forEach((content) => {
+    // Nếu có thẻ con <mark>
+    if (content.querySelector('mark')) {
+      // Lấy nội dung của phần tử mark
+      const markContent = content.querySelector('mark').innerText;
+      // Thay thế nội dung của phần tử content
+      content.innerHTML = markContent;
+    }
+  });
+};
+
 const getResult = (val) => {
   const messagesString = localStorage.getItem('messages');
   const messages = messagesString ? JSON.parse(messagesString) : [];
   const mesResult = [];
   if (messages.length == 0) {
-    const contents = document.querySelectorAll('.content');
-    contents.forEach(function (content) {
-      // Nếu có thẻ con <mark>
-      if (content.querySelector('mark')) {
-        // Lấy nội dung của phần tử mark
-        var markContent = content.querySelector('mark').innerText;
-        // Thay thế nội dung của phần tử content
-        content.innerHTML = markContent;
-      }
-    });
+    clearMarkTag();
     return;
   }
   for (const m of messages) {
