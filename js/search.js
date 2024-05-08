e8fa1d46fe998c629f781451887bced7b9796edc
theanishtar <dangtt135@gmail.com>
2024-05-08 13:33:29 +0700

    fix: clear mark tag in search empty

--- js/search.js
+++ js/search.js
@@ -4,6 +4,19 @@ const getResult = (val) => {
   const messagesString = localStorage.getItem('messages');
   const messages = messagesString ? JSON.parse(messagesString) : [];
   const mesResult = [];
+  if (messages.length == 0) {
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(function (content) {
+      // Nếu có thẻ con <mark>
+      if (content.querySelector('mark')) {
+        // Lấy nội dung của phần tử mark
+        var markContent = content.querySelector('mark').innerText;
+        // Thay thế nội dung của phần tử content
+        content.innerHTML = markContent;
+      }
+    });
+    return;
+  }
   for (const m of messages) {
     if (m.content.includes(val)) {
       mesResult.push(m);
